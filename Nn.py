badae23cf1a194023323352dbf37aae86a37fb3b
Ossi <pera@student.tut.fi>
2019-02-03 10:04:49 +0200

    Refactoring and bugfix

--- Nn.py
+++ Nn.py
@@ -15,66 +15,66 @@ class Nn():
         assert(0 <= epsilon <= 1)
 
         # Discount factor
-        self.__gamma = 0.99
-        self.__learning_rate = learnign_rate
-        self.__keep_prob = keep_prob
-        self.__greedy = greedy
-        self.__epochs = epochs
-        self.__epsilon = epsilon
+        self._gamma = 0.99
+        self._learning_rate = learnign_rate
+        self._keep_prob = keep_prob
+        self._greedy = greedy
+        self._epochs = epochs
+        self._epsilon = epsilon
         self.batch_size = batch_size
 
-        self.__episodes = []
-        self.__episode = { "observations": [], "actions": [], "reward": 0 }
+        self._episodes = []
+        self._episode = { "observations": [], "actions": [], "reward": 0 }
 
-        self.__model_path = "./model/"
-        self.__model_name = "model"
-        self.__verbose = verbose
+        self._model_path = "./model/"
+        self._model_name = "model"
+        self._verbose = verbose
         if verbose:
             np.set_printoptions(precision=3, suppress=True, threshold=np.nan, linewidth=np.nan)
-        self.__board_size = 25
+        self._board_size = 25
         # self.__tensorboard()
-        self.__session = tf.Session()
-        self.__saver = None
+        self._session = tf.Session()
+        self._saver = None
         self.__load_model()
 
     def __str__(self):
         return 'NN'
 
     def new_game(self):
-        self.__episodes.append(self.__episode)
-        self.__episode = { "observations": [], "actions": [], "reward": 0 }
+        self._episodes.append(self._episode)
+        self._episode = { "observations": [], "actions": [], "reward": 0 }
 
     def train(self):
         # Load might have been changed by previous Ai
         self.__load_model()
-        for _ in range(0, self.__epochs):
-            for batch in self.__episodes:
+        for _ in range(0, self._epochs):
+            for batch in self._episodes:
                 rewards = self.discount(batch["reward"], len(batch["actions"]))
-                self.__session.run([self.__loss, self.__training_optimizer],
+                self._session.run([self._loss, self._training_optimizer],
                     {
-                        self.__input: np.vstack(batch["observations"]),
-                        self.__output: np.stack(batch["actions"]),
-                        self.__rewards: rewards,
-                        self.__dropout: self.__keep_prob
+                        self._input: np.vstack(batch["observations"]),
+                        self._output: np.stack(batch["actions"]),
+                        self._rewards: rewards,
+                        self._dropout: self._keep_prob
                     }
                 )
 
         self.__save_model()
-        self.__episodes = []
+        self._episodes = []
 
     def predict(self, data):
         input_board, output_board = self.__preprocess(data)
-        prediction = self.__session.run(self.__softmax, { self.__input: input_board })
+        prediction = self._session.run(self._sigmoid, { self._input: input_board })
         action_index = self.__action_from_prediction(output_board, prediction)
         index = self.__play_action(output_board, action_index)
         coordinates = self.__index_to_coordinates(index)
 
-        action = np.zeros(self.__board_size**2, dtype=np.uint8)
+        action = np.zeros(self._board_size**2, dtype=np.uint8)
         action[action_index] = 1
-        self.__episode["actions"].append(action)
-        self.__episode["observations"].append(input_board)
+        self._episode["actions"].append(action)
+        self._episode["observations"].append(input_board)
 
-        if (self.__verbose):
+        if (self._verbose):
             print('-'*80)
             print('-'*80)
             print('Board')
@@ -85,7 +85,7 @@ class Nn():
         return coordinates
 
     def reward(self, reward):
-        self.__episode["reward"] = reward
+        self._episode["reward"] = reward
 
     # TODO: This should be in completely another class
     def calc_reward(self, reward, turns):
@@ -101,7 +101,7 @@ class Nn():
         discounted_rewards = np.zeros(turns)
         cumulative = 0.0
         for index, reward in enumerate(rewards):
-            cumulative = cumulative * self.__gamma + reward
+            cumulative = cumulative * self._gamma + reward
             discounted_rewards[index] = cumulative
 
         discounted_rewards = np.flip(discounted_rewards)
@@ -109,7 +109,7 @@ class Nn():
         return discounted_rewards
 
     def __index_to_coordinates(self, index):
-        size = self.__board_size
+        size = self._board_size
         c = {
             "x": index % size,
             "y": int(index / size)
@@ -126,13 +126,13 @@ class Nn():
         assert(prediction.shape == (1,625))
         prediction[board != 0] = 0
         action = 0
-        if (self.__greedy):
+        if (self._greedy):
             # Take first best
             # action = np.where(prediction == np.max(prediction))[1][0]
             # Take one of the best
             indexes = np.where(prediction == np.max(prediction))[1]
             action = np.random.choice(indexes)
-        elif (self.__epsilon <= random.random()):
+        elif (self._epsilon <= random.random()):
             prediction_sum = prediction[0].sum()
             normalized = prediction[0]/prediction_sum
             # Boltzman approach
@@ -153,7 +153,7 @@ class Nn():
                 # If invalid spot, play random.
                 return np.random.choice(np.where(board == 0)[1])
             except IndexError:
-                np.savetxt("board.log", np.reshape(board, (self.__board_size, self.__board_size)), "%d")
+                np.savetxt("board.log", np.reshape(board, (self._board_size, self._board_size)), "%d")
                 raise IndexError("Every slot has been played!")
 
     def __tensorboard(self):
@@ -184,54 +184,54 @@ class Nn():
         # [num_channels, first_dim, second_dim] => [first_dim, second_dim, num_channels]
         input_board = np.moveaxis(input_board, 0, -1)
         input_board = input_board[np.newaxis, ...]
-        output_board = np.reshape(board, (1, self.__board_size**2))
+        output_board = np.reshape(board, (1, self._board_size**2))
 
         return input_board, output_board
 
     def __architechture(self):
-        self.__input = tf.placeholder(shape=(None, self.__board_size, self.__board_size, 9), dtype=tf.float32)
-        self.__output = tf.placeholder(shape=(None, self.__board_size**2), dtype=tf.float32)
-        self.__rewards = tf.placeholder(shape=(None,), dtype=tf.float32)
-        self.__dropout = tf.placeholder(dtype=tf.float32)
+        self._input = tf.placeholder(shape=(None, self._board_size, self._board_size, 9), dtype=tf.float32)
+        self._output = tf.placeholder(shape=(None, self._board_size**2), dtype=tf.float32)
+        self._rewards = tf.placeholder(shape=(None,), dtype=tf.float32)
+        self._dropout = tf.placeholder(dtype=tf.float32)
 
-        layer_1 = tf.layers.conv2d(self.__input, filters=32, kernel_size=10, activation=tf.nn.relu, padding='valid')
-        layer_1 = tf.layers.dropout(layer_1, self.__dropout)
+        layer_1 = tf.layers.conv2d(self._input, filters=32, kernel_size=10, activation=tf.nn.relu, padding='valid')
+        layer_1 = tf.layers.dropout(layer_1, self._dropout)
 
         layer_2 = tf.layers.conv2d(layer_1, filters=32, kernel_size=5, activation=tf.nn.relu, padding='valid')
-        layer_2 = tf.layers.dropout(layer_2, self.__dropout)
+        layer_2 = tf.layers.dropout(layer_2, self._dropout)
 
         layer_3 = tf.layers.conv2d(layer_2, filters=32, kernel_size=3, activation=tf.nn.relu, padding='valid')
-        layer_3 = tf.layers.dropout(layer_3, self.__dropout)
+        layer_3 = tf.layers.dropout(layer_3, self._dropout)
 
         layer_4 = tf.layers.flatten(layer_3)
         layer_4 = tf.layers.dense(layer_4, 1024, tf.nn.relu)
-        layer_4 = tf.layers.dropout(layer_4, self.__dropout)
+        layer_4 = tf.layers.dropout(layer_4, self._dropout)
 
-        self.__layer_out = tf.layers.dense(layer_4, self.__board_size**2, activation=None)
-        self.__softmax = tf.nn.softmax(self.__layer_out)
+        self._layer_out = tf.layers.dense(layer_4, self._board_size**2, activation=None)
+        self._sigmoid = tf.nn.sigmoid(self._layer_out)
 
-        x_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.__layer_out, labels=self.__output)
-        self.__loss = tf.reduce_mean(self.__rewards * x_entropy)
-        self.__training_optimizer = tf.train.AdamOptimizer(self.__learning_rate).minimize(self.__loss)
+        x_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self._layer_out, labels=self._output)
+        self._loss = tf.reduce_mean(self._rewards * x_entropy)
+        self._training_optimizer = tf.train.AdamOptimizer(self._learning_rate).minimize(self._loss)
 
         init = tf.global_variables_initializer()
-        self.__session.run(init)
+        self._session.run(init)
 
     def __load_model(self):
-        if (os.path.isfile(self.__model_path + self.__model_name + '.meta')):
-            if self.__saver is None:
-                self.__saver = tf.train.import_meta_graph(self.__model_path + self.__model_name + '.meta')
-            self.__saver.restore(self.__session, tf.train.latest_checkpoint(self.__model_path))
+        if (os.path.isfile(self._model_path + self._model_name + '.meta')):
+            if self._saver is None:
+                self._saver = tf.train.import_meta_graph(self._model_path + self._model_name + '.meta')
+            self._saver.restore(self._session, tf.train.latest_checkpoint(self._model_path))
 
             # save variables
-            self.__input = tf.get_collection("input")[0]
-            self.__output = tf.get_collection("output")[0]
-            self.__rewards = tf.get_collection("rewards")[0]
-            self.__layer_out = tf.get_collection("output_layer")[0]
-            self.__softmax = tf.get_collection("scaled_output")[0]
-            self.__loss = tf.get_collection("loss")[0]
-            self.__training_optimizer = tf.get_collection('optimizer')[0]
-            self.__dropout =tf.get_collection('dropout')[0]
+            self._input = tf.get_collection("input")[0]
+            self._output = tf.get_collection("output")[0]
+            self._rewards = tf.get_collection("rewards")[0]
+            self._layer_out = tf.get_collection("output_layer")[0]
+            self._sigmoid = tf.get_collection("scaled_output")[0]
+            self._loss = tf.get_collection("loss")[0]
+            self._training_optimizer = tf.get_collection('optimizer')[0]
+            self._dropout =tf.get_collection('dropout')[0]
 
         else:
             self.__architechture()
@@ -239,18 +239,18 @@ class Nn():
             self.__save_model(save_meta=True)
 
     def __save_model(self, save_meta=False):
-        if self.__saver is None:
-            self.__saver = tf.train.Saver()
-        tf.add_to_collection("input", self.__input)
-        tf.add_to_collection("output", self.__output)
-        tf.add_to_collection("rewards", self.__rewards)
-        tf.add_to_collection("output_layer", self.__layer_out)
-        tf.add_to_collection("scaled_output", self.__softmax)
-        tf.add_to_collection("loss", self.__loss)
-        tf.add_to_collection('optimizer', self.__training_optimizer)
-        tf.add_to_collection('dropout', self.__dropout)
+        if self._saver is None:
+            self._saver = tf.train.Saver()
+        tf.add_to_collection("input", self._input)
+        tf.add_to_collection("output", self._output)
+        tf.add_to_collection("rewards", self._rewards)
+        tf.add_to_collection("output_layer", self._layer_out)
+        tf.add_to_collection("scaled_output", self._sigmoid)
+        tf.add_to_collection("loss", self._loss)
+        tf.add_to_collection('optimizer', self._training_optimizer)
+        tf.add_to_collection('dropout', self._dropout)
 
-        self.__saver.save(self.__session, self.__model_path + self.__model_name, write_meta_graph=save_meta)
+        self._saver.save(self._session, self._model_path + self._model_name, write_meta_graph=save_meta)
 
 if __name__ == "__main__":
     ai = Nn()
